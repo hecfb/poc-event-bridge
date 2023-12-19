8678ded5de8229761792862da0bfdb38a4a97f96
hecfb <hectorfb.tech@gmail.com>
2023-12-19 07:59:16 -0500

    implemented new features

--- uwf-poc-event-bridge-a.py
+++ uwf-poc-event-bridge-a.py
@@ -6,34 +6,46 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    s3 = boto3.client('s3')
     eventbridge = boto3.client('events')
-    event_detail = {
-        'source': 'lambda.a',
-        'detail-type': 'message',
-        'detail': json.dumps({'message': 'hELLO wORLD'})
-    }
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
+    download_path = f'/tmp/{file_key}'
 
     try:
-        response = eventbridge.put_events(
-            Entries=[
-                {
-                    'Source': event_detail['source'],
-                    'DetailType': event_detail['detail-type'],
-                    'Detail': event_detail['detail'],
-                },
-            ]
-        )
-        logger.info(f"EventBridge response: {response}")
+        # Download and process the file
+        s3.download_file(source_bucket, file_key, download_path)
+        with open(download_path, 'r') as file:
+            data = json.load(file)
+            for item in data:
+                item['isActive'] = not item['isActive']
+
+        # Upload processed data to S3
+        processed_file_key = f'processed/{file_key}'
+        upload_path = f'/tmp/processed_{file_key}'
+        with open(upload_path, 'w') as file:
+            json.dump(data, file)
+        s3.upload_file(upload_path, source_bucket, processed_file_key)
+
+        # Publish an event to EventBridge with the S3 reference
+        event_detail = {
+            'source': 'uwf-poc-event-bridge-a',
+            'detail-type': 'processedFile',
+            'detail': json.dumps({'bucket': source_bucket, 'key': processed_file_key})
+        }
+        response = eventbridge.put_events(Entries=[{
+            'Source': event_detail['source'],
+            'DetailType': event_detail['detail-type'],
+            'Detail': event_detail['detail']
+        }])
+        logger.info("Event sent to EventBridge: " + json.dumps(response))
         return {
             'statusCode': 200,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps({'message': 'Event sent successfully'})
+            'body': json.dumps({'message': 'File processed and event sent'})
         }
-
     except Exception as e:
-        logger.error(f"Error publishing to EventBridge: {e}")
+        logger.error(f"Error: {e}")
         return {
             'statusCode': 500,
-            'headers': {'Content-Type': 'application/json'},
             'body': json.dumps({'error': str(e)})
         }
--- uwf-poc-event-bridge-b.py
+++ uwf-poc-event-bridge-b.py
@@ -7,24 +7,29 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    bucket_name = 'uwf-poc-bucket'
-
-    logger.info(f"Received event: {event}")
+    target_bucket = 'uwf-poc-bucket'
 
     try:
+        detail = event['detail'] if isinstance(event['detail'], dict) else json.loads(event['detail'])
+        source_bucket = detail['bucket']
+        processed_file_key = detail['key']
+        download_path = f'/tmp/{processed_file_key.split("/")[-1]}'
 
-        message = json.loads(event['detail'])['message']
-        formatted_message = message.title()  
+        # Download the processed file from S3
+        s3.download_file(source_bucket, processed_file_key, download_path)
 
-        logger.info(f"Formatted message: {formatted_message}")
+        # Upload the file to the target bucket
+        final_file_key = f'{processed_file_key}-final'
+        s3.upload_file(download_path, target_bucket, final_file_key)
 
-        file_name = 'message.txt'
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=formatted_message)
-
-        logger.info(f"Message saved to S3 bucket: {bucket_name}")
-
+        logger.info(f'Processed file uploaded to target bucket: {final_file_key}')
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Processed file uploaded to target bucket'})
+        }
     except Exception as e:
-        logger.error(f"Error processing event: {e}")
-        raise e
-
-    return {'status': 'Message saved to S3'}
+        logger.error(f'Error: {e}')
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
